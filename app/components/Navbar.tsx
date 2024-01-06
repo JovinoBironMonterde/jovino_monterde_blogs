f1149861a80a6d1b8b6298260ac499135a82309c
Jovino Monterde <jovinobironmonterde@gmail.com>
2024-01-06 16:37:53 +0800

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="cover-photo h-60 bg-slate-400">
+
+      </div>
+      <div className="profile-container flex justify-between p-4 h-50 bg-zinc-300">
+            <div className="">df</div>
+            <div className="">df</div>
+            <div className="w-40 h-40 border rounded-full flex items-center justify-center -mt-4">df</div>
+            <div className="">df</div>
+            <div className="">df</div>
+      </div>
+      <div className="w-full p-4">sd</div>
+
+    </div>
+  )
+}
+
+export default Navbar
