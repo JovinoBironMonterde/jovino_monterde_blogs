d44872775cc79977a35e13b13bc9885e92fd4a52
Jovino Monterde <jovinobironmonterde@gmail.com>
2024-01-08 22:21:22 +0800

    3rd commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,54 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import CommentIcon from '@mui/icons-material/Comment';
+
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="cover-photo h-60 bg-slate-400">
+    <div className='rounded overflow-hidden'>
+      <div className="cover-photo h-60 bg-slate-200">
 
       </div>
-      <div className="profile-container flex justify-between p-4 h-50 bg-zinc-300">
-            <div className="">df</div>
-            <div className="">df</div>
-            <div className="w-40 h-40 border rounded-full flex items-center justify-center -mt-4">df</div>
-            <div className="">df</div>
-            <div className="">df</div>
-      </div>
-      <div className="w-full p-4">sd</div>
+      <div className="profile-container flex justify-between items-center p-4 h-50 px-4 bg-slate-100">
+        <div className="Like-container text-center ">
+          <div className="icon-container mb-2"><ThumbUpIcon className='icon'/></div>
+          <div className="totalCount">100</div>
+          <div className="titlenameName">Likes</div>
+        </div>
+        <div className="Comment-Container text-center">
+          <div className="icon-container mb-2"><CommentIcon className='icon'/></div>
+          <div className="totalCount">100</div>
+          <div className="titlenameName">Comments</div>
+        </div>
 
+        {/* profile */}
+        <div className="profileImage text-center">
+          <div className="imagecontainer text-center">
+          <div className="image w-40 h-40 border rounded-full">image</div>
+          </div>
+          <div className="namedetails text-center">
+            <div className="profilename">Jovino MOnterde</div>
+            <div className="designation">Frontend Developer</div>
+          </div>
+        </div>
+        <div className="Event-Conatiner">Event</div>
+        <div className="AddPost-Container">Add Post</div>
+      </div>
+      <div className="MenuMainContainer bg-blue-100 w-full px-4 py-2">
+        <div className="flex justify-between items-center">
+          <div className="SearchContainer">
+            <input type="text" className=''  name="" id="Inputsearch" />
+          </div>
+          <div className="menuContainer">
+            <Link className='mx-3' href="/page/profile">Profile</Link>
+            <Link className='mx-3' href="/page/post">Post</Link>
+            <Link className='mx-3' href="/page/gallery">Gallery</Link>
+            <Link className='mx-3' href="/page/follower">Followers</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ app/page/profile/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='PageContainer'>
+     <div className="lg:flex md:block ">
+      <div className="content1 lg:w-[30%] md:w=[100%] rounded bg-slate-400 p-3 mb-3">fg</div>
+      <div className="Content2 lg:w-[70%] md:w=[100%] rounded bg-neutral-400  lg:ml-4 p-3 mb-3">sdf</div>
+     </div>
+    </div>
+  )
+}
+
+export default page
