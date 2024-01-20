b6fb93f4ba48355ade60cf8b9d3b59c6ca437cea
Jovino Monterde <jovinobironmonterde@gmail.com>
2024-01-21 01:30:42 +0800

    second commit3

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -6,8 +6,8 @@ const Topbar = () => {
     <div className='flex justify-between items-center p-3'>
      <div className="logo">sd</div>
      <ul className='flex items-center'>
-        <Link className='toplink' href="">Portfolio</Link>
-        <Link className='toplink' href="">Resume</Link>
+        <Link className='toplink' href="https://portfolio-jovinomonterde-new.vercel.app/">Portfolio</Link>
+        <Link className='toplink' href="https://drive.google.com/file/d/1bf6M3LNK8ONDyYhjW2_t9FFp3diqqesd/view">Resume</Link>
      </ul>
     </div>
   )
